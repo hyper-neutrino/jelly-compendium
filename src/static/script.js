18ef49c5d6466c6cae577be16b66910ee9fe97fc
Alexander Liao <alex.yj.liao@gmail.com>
2021-07-07 19:32:21 -0400

    update script.js

---
+++ src/static/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".sidenav").sidenav();
+  hljs.highlightAll();
+});
+
+function copy_codepage() {
+  navigator.clipboard.writeText($("#the-codepage").attr("data-codepage"))
+      .then(() => console.log("copied codepage to clipboard"))
+      .catch(err => console.log("failed to copy codepage:", err));
+}
